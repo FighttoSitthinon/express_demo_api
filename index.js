0ad663f41dfa153c5467b6b4c656e45419230c1e
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-07 18:16:34 +0700

    fix bug get all

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const service = require('./services/search');
-const connect = require('./db/connect');
-const express = require('express');
+const service =  require('./services/search');
+const connect =  require('./db/connect');
+const express =  require('express');
 const app = express();
 
 
-app.get('/all', async (req, res) => {
-    var result = service.getAll;
-    res.send(result);
-});
-
-app.get('/', async (req, res) => {
-    connect.con.query("SELECT * FROM properties LIMIT 3;", function (err, result) {
-        if (err) throw err;
+/*app.get('/', (req, res) => {
+    service.getAll().then(result =>{
         res.send(result);
     });
-});
+});*/
 
+
+app.get('/', async (req, res) => {
+    var result = await service.getAll();
+    res.send(result);
+});
 
 //PORT
 const port = process.env.PORT || 3000;
--- services/search.js
+++ services/search.js
@@ -1,5 +1,4 @@
 const connect = require('../db/connect');
-
 /*
     Search by uniq Id (?uniq_id=00954d18277acc4bf9080370b1e0bcfe)
     Search by property_type (?property_type=Hotel)
@@ -8,14 +7,13 @@ const connect = require('../db/connect');
     Search by Room Price Range min to max (?room_price=1000-2000)
     Search by Location & radius
 */
-
-async function getAll() {
-    var result = [];
-    await connect.con.query("SELECT * FROM properties LIMIT 3;", function (err, res) {
-        if (err) throw err;
-        result = res;
+function getAll() {
+    return new Promise((resolve, reject)=>{
+        connect.con.query("SELECT * FROM properties LIMIT 3;", function (err, res) {
+            if (err) reject(err);
+            else    resolve(res);
+        });
     });
-    return await result;
 }
 
-exports.getAll = getAll();
+exports.getAll = getAll;
