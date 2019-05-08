468203e7da1460b05c1304d2c674c8ad5d7cf616
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-08 14:04:30 +0700

    update readme and update index.js

--- index.js
+++ index.js
@@ -11,56 +11,14 @@ const app = express();
     Search by Location & radius
 */
 
-app.get('/', async (req, res) => {
+app.get('/all', async (req, res) => {
     let sql = "SELECT * FROM properties LIMIT 10;";
     var result = await service.query(sql);
     res.send(result);
 });
 
 app.get('/query/', async (req, res) => {
-
-    let sql = "SELECT * FROM properties WHERE ";
-    let key = false;
-
-    if(req.query.uniq_id){
-        sql = sql + " uniq_id = '" + req.query.uniq_id + "'";
-        key = true;
-        console.log(sql);
-    }
-
-    if(req.query.property_type){
-        sql = sql + (key == true? "AND": "") +" property_type = '" + req.query.property_type+ "'";
-        key = true;
-        console.log(sql);
-    }
-
-    if(req.query.city){
-        sql = sql + (key == true? "AND": "") + " city = '" + req.query.city+ "'";
-        key = true;
-        console.log(sql);
-    }
-
-    if(req.query.amenities){
-        sql = sql + (key == true? "AND": "") + " amenities = '" + req.query.amenities+ "'";
-        key = true;
-        console.log(sql);
-    }
-
-    if(req.query.room_price){
-        if(req.query.room_price.includes("-")){
-            let price = req.query.room_price.split('-');
-            sql = sql + (key == true? "AND": "") + " room_price BETWEEN '" +price[0]+ "' AND '" +price[1]+ "'";
-        }else{
-            sql = sql + (key == true? "AND": "") + " room_price = '" + req.query.room_price+ "'";
-        }
-        key = true;
-        console.log(sql);
-    }
-
-    if(key != true){
-        sql = sql + " 1 ";
-    }
-
+    let sql = await service.genSQL(req.query); 
     var result = await service.query(sql);
     res.send(result);
 });
--- services/query.js
+++ services/query.js
@@ -9,4 +9,49 @@ function query(sql) {
     });
 }
 
+
+function genSQL(query) {
+    let sql = "SELECT * FROM properties WHERE ";
+    let key = false;
+
+    if(query.uniq_id){
+        sql = sql + " uniq_id = '" + query.uniq_id + "'";
+        key = true;
+    }
+
+    if(query.property_type){
+        sql = sql + (key == true? "AND": "") +" property_type = '" + query.property_type+ "'";
+        key = true;
+    }
+
+    if(query.city){
+        sql = sql + (key == true? "AND": "") + " city = '" + query.city+ "'";
+        key = true;
+    }
+
+    if(query.amenities){
+        sql = sql + (key == true? "AND": "") + " amenities = '" + query.amenities+ "'";
+        key = true;
+    }
+
+    if(query.room_price){
+        if(query.room_price.includes("-")){
+            let price = query.room_price.split('-');
+            sql = sql + (key == true? "AND": "") + " room_price BETWEEN '" +price[0]+ "' AND '" +price[1]+ "'";
+        }else{
+            sql = sql + (key == true? "AND": "") + " room_price = '" + query.room_price+ "'";
+        }
+        key = true;
+    }
+
+    if(key != true){
+        sql = sql + " 1 ";
+    }
+
+    console.log(sql);
+    return sql;
+}
+
+
 exports.query = query;
+exports.genSQL = genSQL;
