e0d6cc77f126810ce684639582bdad7d9400e16e
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-21 17:30:54 +0700

    Update Design

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-const service =  require('./services/query');
 const express =  require('express');
+const func =  require('./function/query');
 const app = express();
 
-/*
-    Search by uniq Id (?uniq_id=00954d18277acc4bf9080370b1e0bcfe)
-    Search by property_type (?property_type=Hotel)
-    Search by City (?city=Kanpur)
-    Search by amenities (?amenities=Parking)
-    Search by Room Price Range min to max (?room_price=1000-2000)
-    Search by Location & radius
-*/
+const queries = require('./routes/query');
 
-app.get('/all', async (req, res) => {
-    let sql = "SELECT * FROM properties LIMIT 10;";
-    var result = await service.query(sql);
-    res.send(result);
+app.use('/', queries);
+
+app.get('/', (req, res) => {
+    res.send('Hello');
 });
-
-app.get('/query/', async (req, res) => {
-    let sql = await service.genSQL(req.query); 
-    var result = await service.query(sql);
-    res.send(result);
-});
-
 //PORT
 const port = process.env.PORT || 3000;
+
 app.listen(3000, () => {
     console.log('lisenning in port '+port+'...');
 });
---
+++ routes/query.js
@@ -0,0 +1,27 @@
+const func =  require('../function/query');
+const express = require('express');
+const router = express.Router();
+
+/*
+    Search by uniq Id (?uniq_id=00954d18277acc4bf9080370b1e0bcfe)
+    Search by property_type (?property_type=Hotel)
+    Search by City (?city=Kanpur)
+    Search by amenities (?amenities=Parking)
+    Search by Room Price Range min to max (?room_price=1000-2000)
+    Search by Location & radius
+*/
+
+router.get('/:row', async (req, res) => {
+    let sql = "SELECT * FROM properties LIMIT "+req.params.row;
+    console.log(sql);
+    var result = await func.query(sql);
+    res.send(result);
+});
+
+router.get('/query/', async (req, res) => {
+    let sql = await func.genSQL(req.query); 
+    var result = await func.query(sql);
+    res.send(result);
+});
+
+module.exports = router;
